d7a6ecc1f7a6b622657763544d0968e7ac8578f5
Jashwanth-cyber <2024eb01692@online.bits-pilani.ac.in>
2025-05-19 10:55:15 +0530

    removed server

--- n-clone/src/Components/Login.jsx
+++ n-clone/src/Components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     const userDetails = { username, password };
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://apis.ccbp.in/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
